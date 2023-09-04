bb55a99f0aeadd2356e8b7c280e28b9189350227
Jaggmort <66931312+Jaggmort@users.noreply.github.com>
2023-09-04 15:47:13 +0300

    add DialogFlow learning script.

--- bot.py
+++ bot.py
@@ -1,9 +1,9 @@
 import os
 
+import dialog_flow
 import dotenv
 import logging
 
-from google.cloud import dialogflow
 from telegram import Update
 from telegram.ext import Updater, MessageHandler, Filters, CallbackContext
 
@@ -18,7 +18,7 @@ logger = logging.getLogger(__name__)
 def answer(update: Update, context: CallbackContext) -> None:
     project_id = os.getenv('GOOGLE_CLOUD_PROJECT')
     telegram_user_id = os.getenv('TELEGRAM_CHAT_ID')
-    msg, _ = detect_intent_texts(
+    msg, _ = dialog_flow.detect_intent_texts(
         project_id,
         telegram_user_id,
         update.message.text,
@@ -27,27 +27,14 @@ def answer(update: Update, context: CallbackContext) -> None:
     update.message.reply_text(msg)
 
 
-def detect_intent_texts(project_id, session_id, text, language_code):
-    session_client = dialogflow.SessionsClient()
-    session = session_client.session_path(project_id, session_id)
-
-    text_input = dialogflow.TextInput(text=text, language_code=language_code)
-    query_input = dialogflow.QueryInput(text=text_input)
-    response = session_client.detect_intent(
-        request={"session": session, "query_input": query_input}
-    )
-
-    intent_text = response.query_result.fulfillment_text
-    is_fallback = response.query_result.intent.is_fallback
-    return intent_text, is_fallback
-
-
 def main() -> None:
     dotenv.load_dotenv('.env')
     tg_token = os.environ['TELEGRAM_TOKEN']
     updater = Updater(tg_token)
     dispatcher = updater.dispatcher
-    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, answer))
+    dispatcher.add_handler(
+        MessageHandler(Filters.text & ~Filters.command, answer)
+        )
     updater.start_polling()
     updater.idle()
 
---
+++ dialog_flow.py
@@ -0,0 +1,85 @@
+import os
+import json
+import argparse
+
+from google.cloud import dialogflow
+from dotenv import load_dotenv
+
+
+def get_questions_from_file(file_name):
+    with open(file_name, "r", encoding="UTF-8") as questions_file:
+        questions = json.loads(questions_file.read())
+    return questions
+
+
+def detect_intent_texts(project_id, session_id, text, language_code):
+    session_client = dialogflow.SessionsClient()
+    session = session_client.session_path(project_id, session_id)
+
+    text_input = dialogflow.TextInput(text=text, language_code=language_code)
+    query_input = dialogflow.QueryInput(text=text_input)
+    response = session_client.detect_intent(
+        request={"session": session, "query_input": query_input}
+    )
+
+    intent_text = response.query_result.fulfillment_text
+    is_fallback = response.query_result.intent.is_fallback
+    return intent_text, is_fallback
+
+
+def create_intent(
+        project_id,
+        display_name,
+        training_phrases_parts,
+        message_texts
+        ):
+    intents_client = dialogflow.IntentsClient()
+    parent = dialogflow.AgentsClient.agent_path(project_id)
+    training_phrases = []
+    for training_phrases_part in training_phrases_parts:
+        part = dialogflow.Intent.TrainingPhrase.Part(
+            text=training_phrases_part
+            )
+        training_phrase = dialogflow.Intent.TrainingPhrase(parts=[part])
+        training_phrases.append(training_phrase)
+
+    text = dialogflow.Intent.Message.Text(text=message_texts)
+    message = dialogflow.Intent.Message(text=text)
+
+    intent = dialogflow.Intent(
+        display_name=display_name,
+        training_phrases=training_phrases,
+        messages=[message]
+    )
+
+    response = intents_client.create_intent(
+        request={"parent": parent, "intent": intent, "language_code": "ru", }
+    )
+
+    print("Intent created: {}".format(response))
+
+
+def main():
+    load_dotenv()
+    parser = argparse.ArgumentParser(
+        description='Скрипт для заполенения DialogFlow вопросами и ожидаемыми ответами'
+    )
+    parser.add_argument(
+        'file_name',
+        nargs='?',
+        default='questions.json',
+        help='Подготовленный .json - файл'
+    )
+    project_id = os.getenv('GOOGLE_CLOUD_PROJECT')
+    questions_array = get_questions_from_file(parser.parse_args().file_name)
+    for question_subject, questions in questions_array.items():
+        create_intent(
+            project_id,
+            question_subject,
+            questions['questions'],
+            [questions['answer']]
+            )
+
+
+if __name__ == '__main__':
+    main()
