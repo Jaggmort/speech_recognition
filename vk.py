0a9411c50c81ffa0e2cdc1c387a80bd496047532
Jaggmort <66931312+Jaggmort@users.noreply.github.com>
2023-09-04 16:09:15 +0300

    Add vk bot

--- vk.py
+++ vk.py
@@ -1,17 +1,28 @@
+import os
+
 import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
+from dotenv import load_dotenv
 
-vk_session = vk_api.VkApi(token="vk1.a.I5vL60r5VSgl8JWBZYvHQSDAstzs8bMtjNAAqGYhcGyi6iVRVmy0A1JpNWqaKgSz9TpDOhR_hc91KGQTTtAX_MXBGVlqDo_Fw-INVZwV4RpKKzv9iZsgNe-k-dZCtGJXCZNc86VSRnIn48RyH9PKo9IVTz7lEdJnV1XrSLqEKWq02u3cu7ZXVLjLzOrIh8s8RCFml0ixumSdBD7TAsQCgQ")
 
-longpoll = VkLongPoll(vk_session)
+def main()
+    load_dotenv()
+    vk_chat_token = os.getenv('VK_CHAT_TOKEN')
+    vk_session = vk_api.VkApi(token=vk_chat_token)
 
-for event in longpoll.listen():
-    if event.type == VkEventType.MESSAGE_NEW:
-        print('Новое сообщение:')
-        if event.to_me:
-            print('Для меня от: ', event.user_id)
-            id = event.chat_id
-            vk_session.method('messages.send', {'chat_id': id, 'message': event.text, 'random_id': 0})
-        else:
-            print('От меня для: ', event.user_id)
-        print('Текст:', event.text)
+    longpoll = VkLongPoll(vk_session)
+
+    for event in longpoll.listen():
+        if event.type == VkEventType.MESSAGE_NEW:
+            print('Новое сообщение:')
+            if event.to_me:
+                print('Для меня от: ', event.user_id)
+                id = event.chat_id
+                vk_session.method('messages.send', {'chat_id': id, 'message': event.text, 'random_id': 0})
+            else:
+                print('От меня для: ', event.user_id)
+            print('Текст:', event.text)
+
+
+if __name__ == '__main__':
+    main()
